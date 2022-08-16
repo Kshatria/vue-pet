1ab74317ef2545fb8ca01f54d5acbcdb8838a6aa
Konstantin Budylin <hts7808@gmail.com>
2022-08-16 18:39:06 +0300

    create vue project

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import Register from '@/views/Register'
+import Login from '@/views/Login'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHistory()
+})
+
+export default router
